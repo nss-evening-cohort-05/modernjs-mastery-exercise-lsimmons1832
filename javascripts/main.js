8f20732525b76943981d4a12ec8816cda9ff761c
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-26 18:23:36 -0500

    returning data from PromiseAll

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,39 @@
-console.log("I'm linked");
+$(document).ready(function() {
+
+  const loadCharacters = () => {
+      return new Promise((resolve, reject) => {
+        $.ajax("/db/characters.json")
+        .done((data1) => resolve(data1))
+        .fail((error1) => reject(error1));
+        });
+  };
+
+   const loadGenders = () => {
+      return new Promise((resolve, reject) => {
+          $.ajax("/db/genders.json")
+          .done((data2) => resolve(data2))
+          .fail((error2) => reject(error2));
+      });
+  };
+
+     const loadTeams = () => {
+      return new Promise((resolve, reject) => {
+          $.ajax("/db/teams.json")
+          .done((data3) => resolve(data3))
+          .fail((error3) => reject(error2));
+      });
+  };
+
+Promise.all([loadCharacters(), loadGenders(), loadTeams()])
+	.then((data) => {
+			myComics = data;
+			console.log(myComics);
+			}).catch((errors) => {
+				alert(errors);
+			console.log(errors);
+		});
+	
+
+
+
+});
