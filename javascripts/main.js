a882fcef0e5ea719d0589a360c67c5073039531c
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-26 22:20:14 -0500

    save progress

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+	const myCharacters = [];
+	const myDetails = [];
+	let xmen = [];
+
+const outputContainer = $(".container");
+
+
 
 	$('body').on("click","nav", (e)=>{
 		//determine which button was clicked
@@ -14,24 +21,26 @@ $(document).ready(function() {
 	});
 
 	const getXMen = () =>{
-		//loop over myComics array and grab XMen
-		console.log("I called xmen");
+		//loop over characters array and grab XMen
+	
+
 	};
+// console.log("what's in myComicBook?", myComicBook);
 
 	const getAvengers = () =>{
-		//loop over myComics array and grab Avengers
+		//loop over characters array and grab Avengers
 		console.log("I called avengers");
 	};
 
 	const getGuardians = () =>{
-		//loop over myComics array and grab Guardians
+		//loop over characters array and grab Guardians
 		console.log("I called guardians");
 	};
 
   const loadCharacters = () => {
       return new Promise((resolve, reject) => {
         $.ajax("/db/characters.json")
-        .done((data1) => resolve(data1))
+        .done((data1) => resolve(data1.characters))
         .fail((error1) => reject(error1));
         });
   };
@@ -39,7 +48,7 @@ $(document).ready(function() {
    const loadGenders = () => {
       return new Promise((resolve, reject) => {
           $.ajax("/db/genders.json")
-          .done((data2) => resolve(data2))
+          .done((data2) => resolve(data2.genders))
           .fail((error2) => reject(error2));
       });
   };
@@ -47,21 +56,59 @@ $(document).ready(function() {
      const loadTeams = () => {
       return new Promise((resolve, reject) => {
           $.ajax("/db/teams.json")
-          .done((data3) => resolve(data3))
+          .done((data3) => resolve(data3.teams))
           .fail((error3) => reject(error2));
       });
   };
 
-const myComics = [];
+  const checkTeam = (characters, teams) => { 
+		const formAlliance = characters.team_id;
+    const isMatchNumber = teams.id; 
+    if (isMatchNumber === -1){ 
+      return false;
+    } else {
+      return true;
+    }
+  };
 
-Promise.all([loadCharacters(), loadGenders(), loadTeams()])
+  const checkGender = (characters, genders) =>{
+  	const gender = characters.gender_id;
+  	const gender_id = genders.id;
+  	let isMatch = true;
+  	if(gender && !gender_id){
+  		isMatch = false;
+  	}
+  	return isMatch;
+  };
+
+    loadCharacters().then((characters) => {
+    characters.forEach((character) => {
+      character.matches = []; 
+      myCharacters.push(character);
+    });
+
+
+Promise.all([loadGenders(), loadTeams()])
 	.then((data) => {
-			myComics.push(data);
-			console.log(myComics);
+		data.forEach((dataSet)=>{
+			// dataSet.forEach((detail) =>{
+			myDetails.push(dataSet);
+			// });
+		});
+		for(let i = 0; i < myCharacters.length; i++){
+			for (let j = 0; j < myDetails.length; j++) {
+				if(checkTeam(myCharacters[i], myDetails[j])&& checkGender(myCharacters[i], myDetails[j])){
+					myCharacters[i].matches.push(myDetails[j]);
+				}
+			}
+		}
+			console.log("this is the Array",myCharacters);
+			//writeToDom(myCharacters);
 			}).catch((errors) => {
 				alert(errors);
 			console.log(errors);
 		});
+	});
 	
 
 
