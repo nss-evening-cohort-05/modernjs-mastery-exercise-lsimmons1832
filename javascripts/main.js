a733f55979302fe9d6afaeb9556676bf58f1d8cb
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-09 18:09:32 -0500

    wrote the DOM function over, and changed the css file

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,31 +3,30 @@ $(document).ready(() => {
 
 
 $( "body" ).addClass( "background" );
-$('.navbar-right').html(`<button type="submit" class="btn btn-default" id="xmen">X-Men</button>
-			    <button type="submit" class="btn btn-default" id="avengers">The Avengers</button>
-			    <button type="submit" class="btn btn-default" id="gaurdians">Guardians of the Galaxy</button>`);
+$('.navbar-right').html(`<button type="submit" class="btn btn-default" id="0">X-Men</button>
+			    <button type="submit" class="btn btn-default" id="1">The Avengers</button>
+			    <button type="submit" class="btn btn-default" id="2">Guardians of the Galaxy</button>`);
 
 
 
 
-
-//need to work on rewriting the DOM using Panels & rows instead of the grid format
 	const writeToDom = (data, buttonId)=>{
 		let outputString = "";
 
+			console.log("button", data);
 		for(let i = 0; i < data.length; i++){
-
-			outputString += `<div class="col-xs-6 col-sm-3 card">`;
-			outputString += `<section><h2>${data[i].name}</h2>`;
-			if(data[i].gender_id === 0){
-			outputString += `<img src='${data[i].image}' class="img-circle img-responsive female-img" alt="Responsive image">`;
-			outputString += `<p class="female">${data[i].description}</p></section></div>`;
-			}else{
-			outputString += `<img src='${data[i].image}' class="img-circle img-responsive male-img" alt="Responsive image">`;
-			outputString += `<p class="male">${data[i].description}</p></section></div>`;
+			if(data[i].team_id == buttonId){
+			outputString += `<div class="panel panel-default col-xs-3">`;
+			// outputString += `<div class="panel-body">`;
+			outputString += `<div class="panel-heading"><h2>${data[i].name}</h2></div>`;
+			if(data[i].gender === "Female"){
+			outputString += `<div class="panel-body"><img src='${data[i].image}' class="img-circle img-responsive female-img" alt="Responsive image">`;
+			}else if(data[i].gender === "Male"){
+			outputString += `<div class="panel-body"><img src='${data[i].image}' class="img-circle img-responsive male-img" alt="Responsive image">`;
 			}	
+			outputString += `<p>${data[i].description}</p></div></div>`;
 		}
-
+	}
 		$(".outputContainer").html(outputString);
 	};
 
@@ -67,40 +66,40 @@ Promise.all([loadCharacters(), loadGenders(), loadTeams()])
 			teams.forEach((teams) =>{
 			gender.forEach((genders)=>{
 				if(char.team_id === teams.id){
-					char.team_name = teams.name
+					char.team_name = teams.name;
 				}
 				if(char.gender_id === genders.id){
 					char.gender = genders.type;
 				}	
 				if(char.description === '' && char.gender === 'Female'){
-					char.description = "abcde fghij klmno pqrst uvwxy z"
+					char.description = "abcde fghij klmno pqrst uvwxy z";
 				}else if(char.description === '' && char.gender === 'Male'){
 					char.description = '1234567890';
 				}
 			   });
 			 });
+		  });
 			writeToDom(character, buttonId);
-		   });
 		 }).catch((errors) => {
 			console.log(errors);
 		});
-}
+};
 
-  $("#xmen").click((event) => {
+  $("#0").click((event) => {
 	dataGetter($(event.currentTarget)[0].id);
     console.log($(event.currentTarget));
 	$("body").removeClass("background");
   });
   
-   $("#avengers").click((event) => {
-	dataGetter($(event.currentTarget)[1].id);
+   $("#1").click((event) => {
+	dataGetter($(event.currentTarget)[0].id);
     console.log($(event.currentTarget));
 	$("body").removeClass("background");
   });
   
-    $("#gaurdians").click((event) => {
-	dataGetter($(event.currentTarget)[2].id);
-    console.log($(event.currentTarget));
+    $("#2").click((event) => {
+	dataGetter($(event.currentTarget)[0].id);
+    console.log("I clicked here",$(event.currentTarget));
 	$("body").removeClass("background");
   });
 
